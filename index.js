6a28e843d0b424b7e3f04e730587bac2a11da5f7
Rhilip <admin@rhilip.info>
2022-04-04 19:15:05 +0800

    update: update webextension-polyfill to 0.9

--- index.js
+++ index.js
@@ -84,14 +84,6 @@ module.exports = (api, options) => {
       },
     ])
 
-    // Workaround for https://github.com/mozilla/webextension-polyfill/issues/68
-    webpackConfig.module
-      .rule('provide-webextension-polyfill')
-      .test(require.resolve('webextension-polyfill', { paths: [appRootPath] }))
-      .use('imports')
-      .loader('imports-loader')
-      .options({ additionalCode: 'var browser = undefined;' })
-
     if (isProduction) {
       // Silence warnings of known large files, like images, sourcemaps, and the zip artifact
       webpackConfig.performance.assetFilter((assetFilename) =>
